1564e2eab60f33c18bc36830c16d8bb21a3e604b
Dimwahyu <93499800+Dimwahyu15@users.noreply.github.com>
2022-11-03 13:08:09 +0700

    tailwind import

---
+++ cloud_website/src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className='w-screen h-[80px z-10 bg-zinc-200 fixed drop-shadow-lg' >
+            <div className="px-2 flex justify-beetween items-center w-full h-full">
+                <div className="flex items-center">
+                    <h1 className="text-3l font-bold mr-4 sm:text-4xl">CLOUDS.</h1>
+                    <ul>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Support</li>
+                        <li>Platforms</li>
+                        <li>Pricing</li>
+                    </ul>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Navbar
